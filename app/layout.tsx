1358ff6c90a7055d687a55e29a55b47e0df167b1
ninelinedev <admin@9line.dev>
2025-12-28 03:54:24 +0800

    Update app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import Footer from '@/components/Footer';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Unlock Your Business Potential - Professional business solutions Services',
+  title: 'Elevate Your Business with Our Expertise - Professional business solutions Services',
   description: 'Trusted business solutions services with expertise you can rely on.',
 };
 
